282554dc908e23592c8725ad6936844958098dd4
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-04-12 16:19:40 -0400

    Update EnrolmentResults.java

--- EnrolmentResults.java
+++ EnrolmentResults.java
@@ -1,5 +1,129 @@
+import java.util.Scanner;
 
 public class EnrolmentResults {
+
+	private static void pause(Scanner userIn) {
+		System.out.println("\nAdvance code? Hit enter");
+		userIn.nextLine();
+	}
+
+	public static void main(String[] args) {
+
+		Scanner userIn = new Scanner(System.in);
+
+		// part a
+		// two empty course lists
+		System.out.println("PART A : ");
+		System.out.println("================================================");
+		CourseList cl1 = new CourseList();
+		CourseList cl2 = new CourseList();
+		cl1.display();
+		cl2.display();
+		System.out.println("================================================");
+		pause(userIn);
+
+		// part b
+		// syllabus file
+		System.out.println("PART B : ");
+		System.out.println("================================================");
+		Syllabus s = new Syllabus("Syllabus.txt");
+		System.out.println("================================================");
+		pause(userIn);
+
+		// part c
+		// open a request file
+		System.out.println("PART C : ");
+		System.out.println("================================================");
+
+		System.out.println("================================================");
+		pause(userIn);
+
+		// part d
+		// prompt user for courseIDs, search list, display iterations
+		System.out.println("PART D : ");
+		System.out.println("================================================");
+
+		System.out.println("================================================");
+		pause(userIn);
+
+		// part e
+		// test constructors/methods of classes
+		System.out.println("PART E : ");
+		System.out.println("================================================");
+		
+		System.out.println("Making a new course list as follows, into ecL:");
+		Course ec1 = new Course("ARTS446", "It's the arts", 4, "ARTS244", "ORGN412");
+		Course ec2 = new Course("TECH336", "Bits and Bites", 3, "TECH222", "");
+		Course ec3 = new Course("ARTS412", "Art of origami", 4, "ARTS244", "ARTS446");
+		Course ec4 = new Course("ARTS512", "Advanced art", 6, "ARTS446", "");
+		Course ec5 = new Course("ARTS532", "Advanced art", 6, "ARTS446", "");
+		CourseList ecL = new CourseList();
+		ecL.addToStart(ec1);
+		ecL.addToStart(ec2);
+		ecL.addToStart(ec3);
+		ecL.display();
+
+		pause(userIn);
+		
+		System.out.println("Copying this courselist into ecLPRIME");
+		CourseList ecLPRIME = new CourseList(ecL);
+		ecLPRIME.display();
+		System.out.println("Contents of ecLPRIME (compare this to the previous)");
+		
+		pause(userIn);
+
+		System.out.println("\nAdding ec4 to list at index 4....");
+		ecL.insertAtIndex(ec4, 2);
+		ecL.display();
+
+		pause(userIn);
+
+		System.out.println("ARTS412 is found within this course list ecL is : " + ecL.contains("ARTS412"));
+		System.out.println("PIE345 is found within this course list ecL is : " + ecL.contains("PIE345"));
+
+		// uncomment this to see what happens in the code!
+		// System.out.println("\nAdding ec2 again to list at index 6....");
+		// ecL.insertAtIndex(ec4, 6);
+		// ecL.display();
+
+		pause(userIn);
+
+		System.out.println("Deleting element at index 3");
+		ecL.deleteAtIndex(3);
+		ecL.display();
+
+		pause(userIn);
+
+		System.out.println("Deleting element from start");
+		ecL.deleteFromStart();
+		ecL.display();
+
+		pause(userIn);
+
+		System.out.println("Deleting element from start, AGAIN");
+		ecL.deleteFromStart();
+		ecL.display();
+
+		pause(userIn);
+
+		System.out.println("Deleting element from start, AGAIN");
+		ecL.deleteFromStart();
+		ecL.display();
+
+		pause(userIn);
+
+		System.out.println("ec1.equals(ec2) is " + ec1.equals(ec2));
+		System.out.println("ec4.equals(ec5) is " + ec4.equals(ec5));
+		System.out.println("Cloning ec1 to ec6");
+		Course ec6 = ec1.clone();
+		System.out.println("ec1 is : " + ec1);
+		System.out.println("ec6 (the clone of ec1) is : " + ec6);
+
+		System.out.println("================================================");
+
+		userIn.close();
+
+	}
 
 }
 
